24ec06aa71682e9ea964071353664686e6194b7d
gsc <gsc067218@gmail.com>
2023-03-13 15:42:49 +0800

    测试listview

--- src/Shared/HandyControlDemo_Shared/UserControl/Styles/ListViewDemoCtl.xaml.cs
+++ src/Shared/HandyControlDemo_Shared/UserControl/Styles/ListViewDemoCtl.xaml.cs
@@ -1,9 +1,17 @@
-﻿namespace HandyControlDemo.UserControl;
+﻿using System.Windows.Controls;
+using HandyControlDemo.Data;
+
+namespace HandyControlDemo.UserControl;
 
 public partial class ListViewDemoCtl
 {
     public ListViewDemoCtl()
     {
         InitializeComponent();
+    }
+
+    private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+        System.Diagnostics.Debug.WriteLine(((sender as Button).DataContext as DemoDataModel).Name);
     }
 }
